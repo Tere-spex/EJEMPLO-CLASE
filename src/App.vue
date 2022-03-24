b4d6f511708f674b30c00809fd654f91a0850bfb
CodeOpFE <codeopfe@MacBook-Air.local>
2022-03-24 11:59:51 +0100

    in progress...

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="container mx-auto p-4">
-    <div>Hola</div>
+    <!-- <div>Hola</div> -->
     <!-- 3. utilitzar el component al template -->
-    <input type="number" v-model.number="max1" class="border" />
-    <Counter :max="max1" />
+    <!-- <input type="number" v-model.number="max1" class="border" /> -->
+    <!-- <Counter :max="max1" />
     <Counter :max="10" />
-    <Counter :max="20" />
+    <Counter :max="20" /> -->
+    <Bank/>
   </div>
 </template>
 <script>
 // 1: importar el component
-import HelloWorld from "./components/helloWorld.vue";
-import Counter from "./components/Counter.vue"
+//import HelloWorld from "./components/helloWorld.vue";
+import Bank from "./components/Bank.vue";
+// import Counter from "./components/Counter.vue";
 
 export default {
   // 2: registrar el component
   components: {
-    HelloWorld,
+    //HelloWorld,
     Counter,
+    Bank,
   },
   data() {
     return {
---
+++ src/components/Bank.vue
@@ -0,0 +1,24 @@
+<template>
+  <div><h1>Bank</h1></div>
+  <div>Income: {{income}}€</div>
+  <div>Outcome: {{outcome}}€</div>
+  <div>Balance: {{income + outcome}}€</div>
+</template>
+
+<script>
+export default {
+data(){
+    return {
+        income: 100,
+        outcome: -30,
+    }
+},
+computed(){
+    
+},
+}
+</script>
+
+<style>
+
+</style>
